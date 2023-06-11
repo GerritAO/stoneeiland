22b57326a063694622695a0a55c3618e7f5b84b4
Gerrit <gerritsoerotaroeno@gmail.com>
2023-06-11 18:15:51 -0300

    test

--- usermanagement/src/Users/create_users.java
+++ usermanagement/src/Users/create_users.java
@@ -1,4 +1,5 @@
 package Users;
 
 public class create_users extends users{
+
 }
--- usermanagement/src/Users/users.java
+++ usermanagement/src/Users/users.java
@@ -8,6 +8,7 @@ public class users {
 
         public static List<userObjClass> View(){
                 view_users t = new view_users();
+
                 String query = "SELECT * FROM st_users";
                 List<userObjClass> userList =  t.getUsers(query);
                 return userList;
