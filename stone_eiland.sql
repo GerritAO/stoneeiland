e268c6ef32308c028b8dce12f2d45ad3d1f8f92f
Angelino <angelino.spreeuw@imitsuriname.org>
2023-06-23 21:21:39 -0300

    Update stone_eiland.sql

--- stone_eiland.sql
+++ stone_eiland.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jun 21, 2023 at 11:20 PM
--- Server version: 5.7.36
--- PHP Version: 7.4.26
+-- Generation Time: Jun 24, 2023 at 12:15 AM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,19 +29,21 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `st_activiteiten`;
 CREATE TABLE IF NOT EXISTS `st_activiteiten` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `act_name` varchar(255) DEFAULT NULL,
   `act_price` decimal(10,0) DEFAULT NULL,
-  `status` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `st_activiteiten`
 --
 
-INSERT INTO `st_activiteiten` (`id`, `act_name`, `act_price`, `status`) VALUES
-(1, 'Boswandeling', '50', 'Available');
+INSERT INTO `st_activiteiten` (`id`, `act_name`, `act_price`) VALUES
+(5, 'Boswandeling', '20'),
+(6, 'Kajakken', '30'),
+(7, 'Zwemmen', '25'),
+(8, 'Fietstour', '30');
 
 -- --------------------------------------------------------
 
@@ -51,28 +53,18 @@ INSERT INTO `st_activiteiten` (`id`, `act_name`, `act_price`, `status`) VALUES
 
 DROP TABLE IF EXISTS `st_boekingen`;
 CREATE TABLE IF NOT EXISTS `st_boekingen` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `klant_naam` varchar(255) DEFAULT NULL,
-  `klant_DOB` date DEFAULT NULL,
-  `gender` varchar(255) DEFAULT NULL,
-  `activiteit_naam` varchar(255) DEFAULT NULL,
-  `huis_naam` varchar(255) DEFAULT NULL,
-  `datum_reservering` date DEFAULT NULL,
-  `datum_incheck` date DEFAULT NULL,
-  `datum_uitcheck` date DEFAULT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `huis_id` int DEFAULT NULL,
+  `activ_id` int DEFAULT NULL,
   `reason` varchar(255) DEFAULT NULL,
   `overnight_stay` varchar(255) DEFAULT NULL,
   `status` varchar(255) DEFAULT NULL,
-  `gebruiker` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `st_boekingen`
---
-
-INSERT INTO `st_boekingen` (`id`, `klant_naam`, `klant_DOB`, `gender`, `activiteit_naam`, `huis_naam`, `datum_reservering`, `datum_incheck`, `datum_uitcheck`, `reason`, `overnight_stay`, `status`, `gebruiker`) VALUES
-(1, 'Gerrit', '1993-11-14', 'M', 'Boswandeling', 'Afobakka', '2023-06-21', '2023-06-28', '2023-06-30', NULL, 'yes', 'pending', 'Gerrit');
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `huis_id` (`huis_id`),
+  KEY `activ_id` (`activ_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -82,19 +74,21 @@ INSERT INTO `st_boekingen` (`id`, `klant_naam`, `klant_DOB`, `gender`, `activite
 
 DROP TABLE IF EXISTS `st_huizen`;
 CREATE TABLE IF NOT EXISTS `st_huizen` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `house_name` varchar(255) DEFAULT NULL,
   `house_price` decimal(10,0) DEFAULT NULL,
-  `status` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `st_huizen`
 --
 
-INSERT INTO `st_huizen` (`id`, `house_name`, `house_price`, `status`) VALUES
-(1, 'Suralco Vakantie huis', '200', 'Available');
+INSERT INTO `st_huizen` (`id`, `house_name`, `house_price`) VALUES
+(1, 'AMS-huis', '50'),
+(2, 'KTI-huis', '70'),
+(3, 'JV-huis', '90'),
+(4, 'GS-huis', '110');
 
 -- --------------------------------------------------------
 
@@ -104,7 +98,7 @@ INSERT INTO `st_huizen` (`id`, `house_name`, `house_price`, `status`) VALUES
 
 DROP TABLE IF EXISTS `st_users`;
 CREATE TABLE IF NOT EXISTS `st_users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `user_name` varchar(255) DEFAULT NULL,
   `user_password` varchar(255) DEFAULT NULL,
   `user_lastname` varchar(255) DEFAULT NULL,
@@ -121,7 +115,20 @@ CREATE TABLE IF NOT EXISTS `st_users` (
 INSERT INTO `st_users` (`id`, `user_name`, `user_password`, `user_lastname`, `DOB`, `role`, `status`) VALUES
 (1, 'Gerrit', '123456', 'Soerotaroeno', '1993-11-14', 'Beheerder', 'Actief'),
 (2, 'Keith', '123456789', 'Tjon Kiem Sang', '2003-05-22', 'Gebruiker', 'Actief'),
-(4, 'Angelino', '12345678', 'Spreeuw', '1986-05-01', 'Beheerder', 'Actief');
+(3, 'Jail', '123456789', 'Van Velthuizen', '2023-06-20', 'Gebruiker', 'actief'),
+(4, 'Angelino', '123456789', 'Spreeuw', '1986-05-01', 'Gebruiker', 'Actief');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `st_boekingen`
+--
+ALTER TABLE `st_boekingen`
+  ADD CONSTRAINT `st_boekingen_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `st_users` (`id`),
+  ADD CONSTRAINT `st_boekingen_ibfk_2` FOREIGN KEY (`activ_id`) REFERENCES `st_activiteiten` (`id`),
+  ADD CONSTRAINT `st_boekingen_ibfk_3` FOREIGN KEY (`huis_id`) REFERENCES `st_huizen` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
